d445e0011cf328a9f2c909a31c4493ced95b9c3c
samvitakarkal <samvitaskarkal@gmail.com>
2024-06-06 23:56:26 +0530

    Added index.liquid for users and made it the default page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def index
+    if params[:search]
+      search_query = "%#{params[:search]}%"
+      @users = User.where('first_name LIKE ? OR last_name LIKE ? OR age::text LIKE ? OR gender LIKE ? OR created_at::text LIKE ?',
+                          search_query, search_query, search_query, search_query, search_query)
+    else
+      @users = User.all
+    end
+    @user_count = @users.count
+
+    users_data = @users.map do |user|
+      user.attributes.transform_keys(&:to_s).merge('created_at' => user.created_at)
+    end
+
+    template = Liquid::Template.parse(File.read(Rails.root.join('app', 'views', 'users', 'index.liquid')))
+    render html: template.render('users' => users_data, 'user_count' => @user_count).html_safe
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    update_daily_record(@user)
+    redirect_to users_path
+  end
+
+  private
+
+  def update_daily_record(user)
+    daily_record = DailyRecord.find_or_create_by(date: Date.today)
+    if user.gender == 'male'
+      daily_record.male_count -= 1
+    else
+      daily_record.female_count -= 1
+    end
+    daily_record.save
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
   resources :users, only: [:index, :destroy]
   resources :daily_records, only: [:index]
 end
